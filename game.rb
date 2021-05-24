16a2550b378a7f09b2feb907946c47b43decbd56
Brianna Cerkiewicz <briannac@shaw.ca>
2021-05-24 16:23:56 +0000

    initial commit

---
+++ game.rb
@@ -0,0 +1,18 @@
+class Player
+
+  def initialize
+    @lives = 3
+    @current_player = false
+  end
+
+  end
+
+  class Question
+    def initialize
+      @number1
+      @number2
+    end
+
+    def write_question
+    end
+  end
---
+++ math_game_planning.rb
@@ -0,0 +1,25 @@
+# player--prompts
+# number--parts of the question asked
+# lives--each player gets three
+# turn--who's getting asked the question
+# winner--who's still alive
+# goodbye--game over
+
+class Player
+
+def initialize
+  @lives = 3
+  @current_player = false
+end
+
+end
+
+class Question
+  def initialize
+    @number1
+    @number2
+  end
+
+  def write_question
+  end
+end
